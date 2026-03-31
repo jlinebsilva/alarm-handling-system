a11eb3d8fbd830d737ffa0e3cdcbd606015b3d07
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-03-31 17:47:34 -0300

    refactor: removido lógica misturada com a 'view' da index

--- public/index.php
+++ public/index.php
@@ -1,12 +1,27 @@
-<?php include_once '../components/head.php' ?>
-<?php include_once '../components/header.php' ?>
+<?php include_once '../public/layouts/head.php' ?>
+<?php include_once '../public/layouts/header.php' ?>
 
 <?php 
-  require '../services/connection.php'
+require '../src/Config/connection.php';
+
+$alarms = [];
+$alarmQuery = mysqli_query($connection, "SELECT * FROM alarms");
+
+if ($alarmQuery) {
+    $alarms = mysqli_fetch_all($alarmQuery, MYSQLI_ASSOC);
+}
+
+$equipaments = [];
+$equipQuery = mysqli_query($connection, "SELECT * FROM equipaments");
+
+if ($equipQuery) {
+    $equipaments = mysqli_fetch_all($equipQuery, MYSQLI_ASSOC);
+}
 ?>
 
+<!-- ALARMS -->
 <div class="container mt-4">
-  <?php include '../services/message.php'?>
+  <?php include 'message.php'?>
   <div class="row">
     <div class="col-md-12">
       <div class="card">
@@ -17,6 +32,7 @@
         </div>
 
         <div class="card-body">
+          <?php if (count($alarms) > 0): ?>
           <table class="table table-striped">
             <thead>
               <tr>
@@ -28,86 +44,72 @@
               </tr>
             </thead>
             <tbody>
-            <?php
-              $sql = 'SELECT * FROM alarms';
-              $alarms = mysqli_query($connection, $sql);
-              
-              if (mysqli_num_rows($alarms) > 0) {
-                  foreach($alarms as $alarm) {
-            ?>
+            <?php foreach($alarms as $alarm): ?>
               <tr>
-                <td><?=$alarm['alarm_description'] ?></td>
-                <td><?=date('d/m/Y H:i', strtotime($alarm['alarm_register_date']))?></td>
-                <td><?=$alarm['alarm_equipament'] ?></td>
-                <td><?=$alarm['alarm_classification'] ?></td>
-                <td class="d-flex gap-2">
-                  <a href="alarm-details.php?alarm_id=<?=$alarm['alarm_id'] ?>" class="btn btn-secondary btn-sm">
-                    <span class="bi-eye-fill"></span>&nbsp; Visualizar
+                <td><?= htmlspecialchars($alarm['alarm_description']) ?></td>
+                <td><?= date('d/m/Y H:i', strtotime($alarm['alarm_register_date'])) ?></td>
+                <td><?= htmlspecialchars($alarm['alarm_equipament']) ?></td>
+                <td><?= htmlspecialchars($alarm['alarm_classification']) ?></td>
+                <td>
+                  <a href="alarm-details.php?alarm_id=<?= $alarm['alarm_id'] ?>" class="btn btn-secondary btn-sm">
+                    Visualizar
                   </a>
                 </td>
               </tr>
-              <?php 
-                  }
-              } else {
-                      echo '<h5>Nenhum Alarme Encontrado</h5>';
-                  }
-              ?>
+              <?php endforeach; ?>
             </tbody>
           </table>
+          <?php else: ?>
+            <p>Nenhum Alarme Encontrado</p>
+          <?php endif; ?>
         </div>
       </div>
     </div>
-  </div>  
+  </div>
 
+  <!-- EQUIPAMENTS -->
   <div class="row mt-5">
     <div class="col-md-12">
       <div class="card">
         <div class="card-header">
           <h4>Últimos Equipamentos
-            <a href="equipament-create.php" class="btn btn-primary float-end">Adicionar</a>
+            <a href="/public/equipament-create.php" class="btn btn-primary float-end">Adicionar</a>
           </h4>
         </div>
 
         <div class="card-body">
+          <?php if (count($equipaments) > 0): ?>
           <table class="table table-striped">
             <thead>
               <tr>
-                <th>Número de Série</th>
-                <th>Data de Cadastro</th>
+                <th>Série</th>
+                <th>Data</th>
                 <th>Tipo</th>
                 <th>Ações</th>
               </tr>
             </thead>
             <tbody>
-              <?php
-                $sql = 'SELECT * FROM equipaments';
-                $equipaments = mysqli_query($connection, $sql);
-                
-                if (mysqli_num_rows($equipaments) > 0) {
-                    foreach($equipaments as $equipament) {
-              ?>
+              <?php foreach($equipaments as $equip): ?>
               <tr>
-                <td><?=$equipament['equipament_serie_number'] ?></td>
-                <td><?=date('d/m/Y H:i', strtotime($equipament['equipament_register_date']))?></td>
-                <td><?=$equipament['equipament_type'] ?></td>
-                <td class="d-flex gap-2">
-                  <a href="equipament-details.php?equipament_id=<?=$equipament['equipament_id'] ?>" class="btn btn-secondary btn-sm">
-                    <span class="bi-eye-fill"></span>&nbsp; Visualizar
+                <td><?= htmlspecialchars($equip['equipament_serie_number']) ?></td>
+                <td><?= date('d/m/Y H:i', strtotime($equip['equipament_register_date'])) ?></td>
+                <td><?= htmlspecialchars($equip['equipament_type']) ?></td>
+                <td>
+                  <a href="equipament-details.php?equipament_id=<?= $equip['equipament_id'] ?>" class="btn btn-secondary btn-sm">
+                    Visualizar
                   </a>
                 </td>
               </tr>
-              <?php 
-                  }
-              } else {
-                      echo '<h5>Nenhum Equipamento Encontrado</h5>';
-                  }
-              ?>
+              <?php endforeach; ?>
             </tbody>
           </table>
+          <?php else: ?>
+            <p>Nenhum Equipamento Encontrado</p>
+          <?php endif; ?>
         </div>
       </div>
     </div>
-  </div>  
+  </div>
 </div>
     
-<?php include_once '../components/footer.php' ?>
+<?php include_once '../public/layouts/footer.php' ?>
