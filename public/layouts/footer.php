750c870aded1e26a1b7422bc5d4949e470d533cf
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-04-01 15:01:50 -0300

    style: joga rodapé pro final da tela

--- public/layouts/footer.php
+++ public/layouts/footer.php
@@ -1,4 +1,4 @@
-<div style="position: absolute; bottom: 1; width: 100%; margin-top: 3rem;">
+<div style="position: absolute; bottom: 0; width: 100%; margin-top: 3rem;">
     <footer class="py-3 justify-content-center border-top">
         <p class="text-center text-body-secondary">&copy; 2026 JLineBSilva</p>
     </footer>
