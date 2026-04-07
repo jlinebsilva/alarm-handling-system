329d9e69fd9ddc24961335b5a34cdac893fa84c9
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-04-07 14:04:44 -0300

    refactor: removido arq's 'connection', 'equipament-action' e 'message'

--- services/connection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PSWD', 'root');
-    define('DB', 'alarm_handling_system');
-
-    $connection = mysqli_connect(HOST, USER, PSWD, DB) or die ('Não Foi Possível Conectar');
-?>
